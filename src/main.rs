564f9ae15b0091b5af1dc1906f98fdee8ad28396
Núria <nuria-fl@users.noreply.github.com>
2019-05-29 12:46:40 +0200

    add available items (#1)
    * add available items
    
    * refactor random item

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 extern crate rand;
 
+use rand::seq::SliceRandom;
 use rand::Rng;
 use std::io;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+
+mod items;
+
+use crate::items::{Item, SCAVENGEABLE_ITEMS};
 
 const MAX: f64 = 100.0;
 
@@ -89,11 +94,9 @@ fn main() {
     }
 }
 
-fn scavenge(inv: &mut std::vec::Vec<&str>, stats: &mut Stats) {
-    let items = ["water", "berries", "wood", "flint", "string", "clams"];
+fn scavenge(inv: &mut std::vec::Vec<Item>, stats: &mut Stats) {
     let inv_max = 10;
 
-    // const numberOfItems = getters.slotsInInventoryLeft > 3 ? 3 : getters.slotsInInventoryLeft
     let slots_left = inv_max - inv.len();
     let number_of_items = if slots_left < 3 { slots_left } else { 3 };
 
@@ -103,10 +106,9 @@ fn scavenge(inv: &mut std::vec::Vec<&str>, stats: &mut Stats) {
         let mut rng = rand::thread_rng();
         sleep(Duration::new(2, 0));
         stats.energy.decrease(5.0);
-        for number in 0..number_of_items {
-            let random_idx = rng.gen_range(0, items.len() - 1);
-            let item = items[random_idx];
-            println!("You found {}", item);
+        for _number in 0..number_of_items {
+            let item = SCAVENGEABLE_ITEMS.choose(&mut rng).unwrap().clone();
+            println!("You found {:?}", item.name);
             inv.push(item);
         }
     }
