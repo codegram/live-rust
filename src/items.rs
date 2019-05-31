8ffc6fda9a1cf1aae359c42bc3ac0737095370ec
Nuria <nuria@codegram.com>
2019-05-31 10:06:38 +0200

    items refactor

--- src/items.rs
+++ src/items.rs
@@ -2,6 +2,27 @@ use colored::*;
 use std::fmt;
 
 #[derive(Debug, Clone)]
+pub struct Item {
+    pub id: &'static str,
+    pub name: &'static str,
+    pub description: &'static str,
+    pub properties: ItemProperties,
+}
+
+#[derive(Debug, Clone)]
+pub enum ItemProperties {
+    StandardItem,
+    ConsumeableItem {
+        value: Stats,
+        risk: i32,
+        days_to_perish: i32, // 0 for non perishable items
+    },
+    ToolItem {
+        uses_until_breakdown: i32,
+    },
+}
+
+#[derive(Debug, Clone)]
 pub struct Stats {
     pub health: f64,
     pub water: f64,
@@ -9,31 +30,18 @@ pub struct Stats {
     pub energy: f64,
 }
 
-#[derive(Clone)]
-pub struct Item<'a> {
-    pub id: &'a str,
-    pub name: &'a str,
-    pub description: &'a str,
-    pub value: Stats,
-    risk: i32,
-    pub consumable: bool,
-    days_to_perish: i32, // 0 for non perishable items
-    uses_until_breakdown: i32,
-}
-
-impl<'a> fmt::Display for Item<'a> {
+impl fmt::Display for Item {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.consumable {
-            write!(
+        match &self.properties {
+            ItemProperties::ConsumeableItem { value, .. } => write!(
                 f,
                 "{}: {} (+{} water, +{} food)",
                 self.id.bold(),
                 self.description.dimmed(),
-                self.value.water,
-                self.value.food
-            )
-        } else {
-            write!(f, "{}: {}", self.id.bold(), self.description.dimmed())
+                value.water,
+                value.food
+            ),
+            _ => write!(f, "{}: {}", self.id.bold(), self.description.dimmed()),
         }
     }
 }
@@ -43,136 +51,73 @@ pub const SCAVENGEABLE_ITEMS: [Item; 9] = [
         id: "berries",
         name: "Berries",
         description: "Small amount of vitamins and water",
-        value: Stats {
-            health: 0.0,
-            food: 3.0,
-            water: 3.0,
-            energy: 0.0,
+        properties: ItemProperties::ConsumeableItem {
+            value: Stats {
+                health: 0.0,
+                food: 3.0,
+                water: 3.0,
+                energy: 0.0,
+            },
+            risk: 0,
+            days_to_perish: 5,
         },
-        risk: 0,
-        days_to_perish: 5,
-        uses_until_breakdown: 0,
-        consumable: true,
     },
     Item {
         id: "water-dirty",
         name: "Dirty Water",
         description: "It will calm your thirst, but might make you sick",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 20.0,
-            energy: 0.0,
+        properties: ItemProperties::ConsumeableItem {
+            value: Stats {
+                health: 0.0,
+                food: 0.0,
+                water: 20.0,
+                energy: 0.0,
+            },
+            risk: 6,
+            days_to_perish: 0,
         },
-        risk: 6,
-        days_to_perish: 0,
-        uses_until_breakdown: 0,
-        consumable: true,
     },
     Item {
         id: "salt",
         name: "Salt",
         description: "Useful to preserve food",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
     Item {
         id: "string",
         name: "String",
         description: "Useful for crafting",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
     Item {
         id: "wood",
         name: "Wood",
         description: "Useful for crafting",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
     Item {
         id: "plastic",
         name: "Plastic",
         description: "Useful for crafting",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
     Item {
         id: "bottle",
         name: "Empty bottle",
         description: "Useful for crafting",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
     Item {
         id: "flint",
         name: "Flint",
         description: "Useful for crafting",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
     Item {
         id: "medicinal-herbs",
         name: "Medicinal herbs",
         description: "Healing properties when brewed",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
 ];
 
@@ -181,136 +126,104 @@ pub const CRAFTABLE_ITEMS: [Item; 9] = [
         id: "meat",
         name: "Raw meat",
         description: "Careful, might have parasites!",
-        value: Stats {
-            health: 0.0,
-            food: 20.0,
-            water: 0.0,
-            energy: 0.0,
+        properties: ItemProperties::ConsumeableItem {
+            value: Stats {
+                health: 0.0,
+                food: 20.0,
+                water: 0.0,
+                energy: 0.0,
+            },
+            risk: 6,
+            days_to_perish: 3,
         },
-        risk: 6,
-        days_to_perish: 3,
-        uses_until_breakdown: 0,
-        consumable: true,
     },
     Item {
         id: "jerky",
         name: "Jerky",
         description: "Long lasting nourishment",
-        value: Stats {
-            health: 0.0,
-            food: 20.0,
-            water: 0.0,
-            energy: 0.0,
+        properties: ItemProperties::ConsumeableItem {
+            value: Stats {
+                health: 0.0,
+                food: 20.0,
+                water: 0.0,
+                energy: 0.0,
+            },
+            risk: 0,
+            days_to_perish: 0,
         },
-        risk: 0,
-        days_to_perish: 0,
-        uses_until_breakdown: 0,
-        consumable: true,
     },
     Item {
         id: "medicinal-tea",
         name: "Medicinal tea",
         description: "Cures you from sickness and restores health",
-        value: Stats {
-            health: 50.0,
-            food: 0.0,
-            water: 20.0,
-            energy: 0.0,
+        properties: ItemProperties::ConsumeableItem {
+            value: Stats {
+                health: 50.0,
+                food: 0.0,
+                water: 20.0,
+                energy: 0.0,
+            },
+            risk: 0,
+            days_to_perish: 0,
         },
-        risk: 0,
-        days_to_perish: 0,
-        uses_until_breakdown: 0,
-        consumable: true,
     },
     Item {
         id: "meat-cooked",
         name: "Cooked meat",
         description: "Tasty nourishment",
-        value: Stats {
-            health: 0.0,
-            food: 20.0,
-            water: 0.0,
-            energy: 0.0,
+        properties: ItemProperties::ConsumeableItem {
+            value: Stats {
+                health: 0.0,
+                food: 20.0,
+                water: 0.0,
+                energy: 0.0,
+            },
+            risk: 0,
+            days_to_perish: 10,
         },
-        risk: 0,
-        days_to_perish: 10,
-        uses_until_breakdown: 0,
-        consumable: true,
     },
     Item {
         id: "water-clean",
         name: "Clean water",
         description: "Safe for drink",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 20.0,
-            energy: 0.0,
+        properties: ItemProperties::ConsumeableItem {
+            value: Stats {
+                health: 0.0,
+                food: 0.0,
+                water: 20.0,
+                energy: 0.0,
+            },
+            risk: 0,
+            days_to_perish: 0,
         },
-        risk: 0,
-        days_to_perish: 0,
-        uses_until_breakdown: 0,
-        consumable: true,
     },
     Item {
         id: "rope",
         name: "Rope",
         description: "Useful for crafting",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 0,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
     Item {
         id: "bow:",
         name: "Bow:",
         description: "Lets you hunt and defend yourself:",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
+        properties: ItemProperties::ToolItem {
+            uses_until_breakdown: 5,
         },
-        uses_until_breakdown: 5,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
     },
     Item {
-        id: "knife:",
-        name: "Knife:",
-        description: "Useful tool:",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
+        id: "knife",
+        name: "Knife",
+        description: "Useful tool",
+        properties: ItemProperties::ToolItem {
+            uses_until_breakdown: 5,
         },
-        uses_until_breakdown: 5,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
     },
     Item {
         id: "rabbit-pelt:",
         name: "Rabbit pelt:",
         description: "It's not gonna be of much use until you can craft more stuff…:",
-        value: Stats {
-            health: 0.0,
-            food: 0.0,
-            water: 0.0,
-            energy: 0.0,
-        },
-        uses_until_breakdown: 5,
-        consumable: false,
-        days_to_perish: 0,
-        risk: 0,
+        properties: ItemProperties::StandardItem,
     },
 ];
 
@@ -318,14 +231,5 @@ const HUNTABLE_ITEMS: [Item; 1] = [Item {
     id: "rabbit",
     name: "Dead rabbit",
     description: "Poor little thing",
-    value: Stats {
-        health: 0.0,
-        food: 0.0,
-        water: 0.0,
-        energy: 0.0,
-    },
-    uses_until_breakdown: 0,
-    consumable: false,
-    days_to_perish: 0,
-    risk: 0,
+    properties: ItemProperties::StandardItem,
 }];
