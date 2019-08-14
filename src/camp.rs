c4b64031cb6e6b501c3afabfdf07abf11c5c6408
Núria <nuria-fl@users.noreply.github.com>
2019-08-14 11:41:18 +0200

    change fire status (#10)

--- src/camp.rs
+++ src/camp.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub struct Fire {
   pub status: FireStatus,
+  pub time: i32,
 }
 
 #[derive(Debug, PartialEq)]
@@ -15,13 +16,56 @@ impl Fire {
   pub fn new() -> Self {
     Fire {
       status: FireStatus::Out,
+      time: 0,
     }
   }
   pub fn craft(&mut self) {
     self.status = FireStatus::Hot;
+    self.time = 0;
   }
   pub fn destroy(&mut self) {
     self.status = FireStatus::Out;
+    self.time = 0;
+  }
+  pub fn pass_time(&mut self) {
+    if self.status != FireStatus::Out {
+      self.time += 10;
+
+      if self.time % 60 == 0 {
+        self.lower_status();
+      }
+    }
+  }
+  pub fn lower_status(&mut self) {
+    match self.status {
+      FireStatus::Hot => {
+        println!("Fire is burning");
+        self.status = FireStatus::Regular;
+      }
+      FireStatus::Regular => {
+        println!("Fire is burning low");
+        self.status = FireStatus::Low;
+      }
+      _ => {
+        println!("Fire has burnt out");
+        self.destroy();
+      }
+    }
+  }
+  pub fn increase_status(&mut self) {
+    match self.status {
+      FireStatus::Low => {
+        println!("Fire is burning");
+        self.time = 60;
+        self.status = FireStatus::Regular;
+      }
+      FireStatus::Regular | FireStatus::Hot => {
+        println!("Fire is burning hot");
+        self.time = 0;
+        self.status = FireStatus::Hot;
+      }
+      _ => {}
+    }
   }
 }
 
